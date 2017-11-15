581ae57ee200f82e48d96761a2749a7f45cde39a
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-15 08:29:48 -0500

    Prompt written.

--- CPP-PRG-13-03-Car-Class/main.cpp
+++ CPP-PRG-13-03-Car-Class/main.cpp
@@ -5,11 +5,34 @@
 //  Created by Keith Smith on 11/15/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a class named Car that has the following member variables:
+//
+//  * yearModel - an int that holds the car's year model
+//  * make - a string that holds the make of the car
+//  * speed - an int that holds the car's current speed
+//
+//  In addition, the class should have the following constructor and other meber functions:
+//
+//  * Constructor - The constructor should accept the car's year model and make as
+//  arguments. These values should be assigned to the object's yearModel and make member
+//  variables. The constructor should also assign 0 to the speed member variables.
+//  * Accessor - appropriate accessor functions to get the values stored in an object's
+//  yearModel, make, and speed member variables.
+//  * accelerate - The accelerate function should add 5 to the speed member variable
+//  each time it is called.
+//  * brake - The brake function should substract 5 from the speed member variable
+//  each time it is called.
+//
+//  Demomstrate the class in a program that creates a Car object, then calls the accelerate
+//  function five times. After each call to the accelerate function, get the current speed
+//  of the car and display it. Then, call the brake function five times. After each call to the
+//  brake function, get the current speed of the car and display it.
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
